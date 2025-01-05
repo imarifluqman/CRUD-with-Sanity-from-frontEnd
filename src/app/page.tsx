81d3cb21c997e99893df69177cd94c3e41f75eec
Muhammad Arif <arifluqman477@gmail.com>
2025-01-06 01:18:06 +0500

    correct Image path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,8 @@
 
-import Image from "next/image";
-
-
 export default function Home() {
-
-
-
   return (
     <div className="w-full h-[100vh] flex flex-col justify-center items-center">
-      <Image src="/sanity-logo/sanity-logotype--red-on-white-1.svg" alt="Vercel Logo" width={300} height={24} />
+      <img src="/sanity-logo/sanity-logotype--red-on-white-1.svg" alt="Vercel Logo" width={300} height={24} />
       <h2 className="text-2xl text-red-600 font-bold border-b-2 border-red-600 p-2">Contact Form CRUD operations with Sanity</h2>
     </div>
   )
